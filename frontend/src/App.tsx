c99544c04939b0734fab9fbad6f8c7ba30c72dd7
irshad12 <mhdirshad2k2@gmail.com>
2026-05-04 22:02:30 +0530

    Frontend initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+]);
+
+function App() {
+ return (
+     <RouterProvider router={router} />
+ )
+}
+
+export default App
